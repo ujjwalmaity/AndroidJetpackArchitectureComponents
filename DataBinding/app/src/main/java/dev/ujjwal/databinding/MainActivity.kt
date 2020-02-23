bdc6316590450a057dbf2eb6d5d96a592dc8f806
ujjwal <ujjwalmaity98@gmail.com>
2020-02-24 00:11:36 +0530

    dataBinding instance

--- DataBinding/app/src/main/java/dev/ujjwal/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/dev/ujjwal/databinding/MainActivity.kt
@@ -2,11 +2,18 @@ package dev.ujjwal.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import dev.ujjwal.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val activityMainBinding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        activityMainBinding.tvName.text = "Ujjwal Maity"
+        activityMainBinding.tvEmail.text = "ujjwalmaity98@gmail.com"
     }
 }
