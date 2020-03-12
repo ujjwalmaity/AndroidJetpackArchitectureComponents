5da1b1cc563697aa02801cc862367696e5a02a87
ujjwal <ujjwalmaity98@gmail.com>
2020-03-13 01:16:41 +0530

    update ui components with NavigationUI

--- Navigation/app/src/main/java/dev/ujjwal/navigation/MainActivity.kt
+++ Navigation/app/src/main/java/dev/ujjwal/navigation/MainActivity.kt
@@ -2,11 +2,56 @@ package dev.ujjwal.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        setUpBottomNav(navController)
+        setUpSideNav(navController)
+        setUpActionNav(navController)
+    }
+
+    private fun setUpBottomNav(navController: NavController) {
+        bottomNavView?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    private fun setUpSideNav(navController: NavController) {
+        navView?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    private fun setUpActionNav(navController: NavController) {
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        val navigated = NavigationUI.onNavDestinationSelected(item!!, navController)
+        return navigated || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
--- Navigation/app/src/main/java/dev/ujjwal/navigation/ShopFragment.kt
+++ Navigation/app/src/main/java/dev/ujjwal/navigation/ShopFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_shop.*
 
 class ShopFragment : Fragment() {
 
@@ -14,5 +16,13 @@ class ShopFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shop, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_aboutUs.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.about_destination)
+        }
     }
 }
