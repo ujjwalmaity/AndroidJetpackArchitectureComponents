825454ce7a8b7c5e9d5eee190f070b4fc9aab3f6
ujjwal <ujjwalmaity98@gmail.com>
2020-04-23 12:09:39 +0530

    initial project setup

--- Room/app/src/main/java/dev/ujjwal/room/MainActivity.kt
+++ Room/app/src/main/java/dev/ujjwal/room/MainActivity.kt
@@ -2,11 +2,21 @@ package dev.ujjwal.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var noteViewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener {
+        }
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel::class.java)
     }
 }
---
+++ Room/app/src/main/java/dev/ujjwal/room/NoteViewModel.kt
@@ -0,0 +1,7 @@
+package dev.ujjwal.room
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+}
